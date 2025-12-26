0c0f349d2d05d7c86db4f2a9cbe23a48643a7b56
Medex3 <75747893+Medex3@users.noreply.github.com>
2025-12-26 13:43:58 +0300

    Update settings.py

--- autosalon/settings.py
+++ autosalon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!(*7gi+_1m)z2k2s1+@!tabj%imwf9zlqe)kh!0_dw1vi)xlll'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
